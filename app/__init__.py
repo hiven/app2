2e218b3112f55ce1f2f833af0888fca74e0b8022
hiven <mail@hiven.com>
2021-11-14 20:35:07 +0000

    Update and rename __init__.py to app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,14 +4,6 @@ from main.views import main_bp
 from products.views import products_bp
 
 app = Flask(__name__)
-db = SQLAlchemy()
-
-username = "user"
-password = "password"
-dbname = "ourdb"
-
-app.config["SECRET_KEY"] = 'aaaa'
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://{username}:{password}@localhost:5432/{dbname}"
 
 app.register_blueprint(main_bp)
 app.register_blueprint(products_bp, url_prefix='/product')
