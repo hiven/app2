437c35bc8fdcdd71e0066bbf7c8c1ab20b9116e9
hiven <mail@hiven.com>
2021-11-14 20:38:03 +0000

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,7 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from main.views import main_bp
-from products.views import products_bp
 
 app = Flask(__name__)
 
-app.register_blueprint(main_bp)
-app.register_blueprint(products_bp, url_prefix='/product')
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
